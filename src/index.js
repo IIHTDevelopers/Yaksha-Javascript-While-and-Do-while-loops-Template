86106a46469737d72f087a56d90e02502a50a7c6
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2025-04-21 14:51:31 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// Using while loop to count from 1 to 5
+var count = 1;
+while (count <= 5) {
+    console.log(count);
+    count++;
+}
+
+// Using do-while loop to count from 1 to 5 (it will always run at least once)
+var doCount = 1;
+do {
+    console.log(doCount);
+    doCount++;
+} while (doCount <= 5);
